40d4a450d55cf94bde12cff89f199c9b62d3d0a8
Vyacheslav Bek <slavabek.games@gmail.com>
2024-09-15 20:15:41 +0300

    changes

---
+++ build.bat
@@ -0,0 +1,19 @@
+@echo off
+
+chcp 65001
+
+
+set BUILD_TYPE=Ninja
+set BUILD_SUFFIX=ninja
+
+
+set BUILD_FOLDER=build_%BUILD_SUFFIX%
+
+if not exist %BUILD_FOLDER% mkdir %BUILD_FOLDER%
+
+cd %BUILD_FOLDER%
+
+cmake -G %BUILD_TYPE% ..
+cmake --build .
+
+cmd /k
